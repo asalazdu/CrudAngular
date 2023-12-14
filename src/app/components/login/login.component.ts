68862c3e9bfd37e6fa3d975759c02556208550d5
Alexis <alexizarduque@gmail.com>
2023-12-13 19:04:09 -0500

    Diseño del Login y animación del mismo

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -4,18 +4,8 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
-  animations: [
-    trigger('fadeInOut', [
-      state('void', style({ opacity: 0, transform: 'scale(0.8)' })),
-      state('*', style({ opacity: 1, transform: 'scale(1)' })), 
-      transition(':enter', [
-        animate('0.5s cubic-bezier(0.68, -0.55, 0.27, 1.55)') 
-      ]),
-      transition(':leave', [
-        animate('0.5s cubic-bezier(0.68, -0.55, 0.27, 1.55)')
-      ]),
-    ]),
-  ],
+  styleUrls: ['./login.component.css'] ,
 })
-export class LoginComponent {  }
+export class LoginComponent { 
+  
+ }
